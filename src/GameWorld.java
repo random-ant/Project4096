1b07bc0f6903853827a766c01ea7c90215406975
Nathan lu <pianobear2008@gmail.com>
2025-04-08 14:30:46 -0500

    dynamic sizing of Block and Tiles

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -2,8 +2,11 @@ import mayflower.*;
 
 public class GameWorld extends World {
     private Tile[][] grid;
-    public int GRID_HEIGHT = 10, GRID_WIDTH = 10;
-    public int OFFSET_X = 45, OFFSET_Y = 250;
+
+    public static int GRID_HEIGHT = 10, GRID_WIDTH = 10;
+    public static int OFFSET_X = 45, OFFSET_Y = 250;
+    public static int TILE_WIDTH = 70, TILE_HEIGHT = 70;
+    public static int BLOCK_WIDTH = 60, BLOCK_HEIGHT = 60;
 
     public GameWorld() {
         grid = new Tile[GRID_HEIGHT][GRID_WIDTH];
@@ -18,7 +21,12 @@ public class GameWorld extends World {
 
         for (int i = 0; i < GRID_HEIGHT; i++) {
             for (int j = 0; j < GRID_WIDTH; j++) {
-                addObject(new Tile(), (j * 70) + OFFSET_X, (i * 70) + OFFSET_Y);
+                int x_coord = (j * TILE_WIDTH) + OFFSET_X;
+                int y_coord = (i * TILE_WIDTH) + OFFSET_Y;
+                addObject(new Tile(), x_coord, y_coord);
+
+                Block b = new Block(2, BColor.BLUE);
+                addObject(b, x_coord + (TILE_WIDTH - BLOCK_WIDTH) / 2, y_coord + (TILE_HEIGHT - BLOCK_HEIGHT) / 2);
             }
         }
     }
