0ec68527e9a380a292fe2484adc8df28d71675ea
Nathan lu <pianobear2008@gmail.com>
2025-04-08 14:01:14 -0500

    update tile img

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -3,7 +3,7 @@ import mayflower.*;
 public class GameWorld extends World {
     private Tile[][] grid;
     public int GRID_HEIGHT = 10, GRID_WIDTH = 10;
-    public int OFFSET_X = 45, OFFSET_Y = 200;
+    public int OFFSET_X = 45, OFFSET_Y = 250;
 
     public GameWorld() {
         grid = new Tile[GRID_HEIGHT][GRID_WIDTH];
