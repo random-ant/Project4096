0b8fd338ef6069e76a8b262cbacd5f8ad1436925
Nathan lu <pianobear2008@gmail.com>
2025-04-10 14:35:32 -0500

    add stack algorithm to merging blocks

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -11,7 +11,11 @@ public class GameWorld extends World {
 
     public GameWorld() {
         grid = new Block[GRID_HEIGHT][GRID_WIDTH];
-        grid[1][1] = new Block(2, BColor.BLUE, new Coordinate(1, 1));
+        grid[1][0] = new Block(2, BColor.BLUE, new Coordinate(1, 1));
+        grid[2][0] = new Block(2, BColor.BLUE, new Coordinate(1, 1));
+        grid[3][0] = new Block(4, BColor.BLUE, new Coordinate(1, 1));
+        grid[4][0] = new Block(2, BColor.BLUE, new Coordinate(1, 1));
+        grid[5][0] = new Block(4, BColor.BLUE, new Coordinate(1, 1));
 
         addObject(new Border(), 40, 245);
         addObject(new Title(), 301, 55);
@@ -43,13 +47,71 @@ public class GameWorld extends World {
         // listen for key presses and act accordingly
         if (keyPresssed(Keyboard.KEY_UP)) {
             System.out.println("Up key pressed");
-        } else if (keyPresssed(Keyboard.KEY_DOWN)) {
+
+            for (int j = 0; j < GRID_WIDTH; j++) {
+                Block[] col = new Block[GRID_HEIGHT];
+                for (int i = 0; i < GRID_HEIGHT; i++) {
+                    col[i] = grid[i][j];
+                }
+
+                Block[] result = check(col);
+
+                grid[j] = result;
+            }
+
+            renderGrid();
+
+        } else if (true || keyPresssed(Keyboard.KEY_DOWN)) {
             System.out.println("Down key pressed");
+
+            for (int j = 0; j < GRID_WIDTH; j++) {
+                Block[] col = new Block[GRID_HEIGHT];
+                for (int i = GRID_HEIGHT - 1; i >= 0; i--) {
+                    col[GRID_HEIGHT - 1 - i] = grid[i][j];
+                }
+
+                ArrayList<Block> result = check(col);
+
+                grid[j] = result;
+            }
+
+            renderGrid();
+
+        } else if (keyPresssed(Keyboard.KEY_SPACE)) {
+            System.out.println("Space key pressed");
+            spawnBlock();
+        } else if (keyPresssed(Keyboard.KEY_ESCAPE)) {
+            System.out.println("Escape key pressed");
         } else if (keyPresssed(Keyboard.KEY_LEFT)) {
-            System.out.println("Left key pressed");
         } else if (keyPresssed(Keyboard.KEY_RIGHT)) {
-            System.out.println("Right key pressed");
         }
+
+    }
+
+    // Get list of arrays
+    public ArrayList<Block> check(Block[] blocks) {
+        Stack<Block> stack = new Stack<Block>();
+
+        for (Block b : blocks) {
+            Block currentBlock = b;
+            if (currentBlock == null)
+                continue;
+
+            while (!stack.empty() && stack.peek().getValue() == currentBlock.getValue()) {
+                Block top = stack.pop();
+                Block newBlock = new Block(top.getValue() * 2, BColor.NEUTRAL, top.getCoordinate());
+                currentBlock = newBlock;
+            }
+            stack.add(currentBlock);
+        }
+
+        ArrayList<Block> result = new ArrayList<Block>();
+        // add stack elements to result array
+        while (!stack.empty()) {
+            result.add(stack.pop());
+        }
+
+        return result;
     }
 
     public ArrayList<Coordinate> getEmptyTiles() {
