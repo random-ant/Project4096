989ac7456c3699333ab3d8a54e5370c374c138de
Nathan Lu <pianobear2008@gmail.com>
2025-04-16 19:35:57 -0500

    move file locations for blocks

--- src/Block.java
+++ src/Block.java
@@ -11,19 +11,19 @@ public class Block extends Actor {
         this.color = color;
 
         if (color == BColor.NEUTRAL) {
-            MayflowerImage img = new MayflowerImage("src/img/blocks/neutral-block" + value + ".png");
+            MayflowerImage img = new MayflowerImage("src/img/blocks/neutral/neutral-block" + value + ".png");
             img.scale(GameWorld.BLOCK_WIDTH, GameWorld.BLOCK_HEIGHT);
             setImage(img);
         }
 
         if (color == BColor.BLUE) {
-            MayflowerImage blue = new MayflowerImage("src/img/blocks/blue-block" + value + ".png");
+            MayflowerImage blue = new MayflowerImage("src/img/blocks/blue/blue-block" + value + ".png");
             blue.scale(GameWorld.BLOCK_WIDTH, GameWorld.BLOCK_HEIGHT);
             setImage(blue);
         }
 
         if (color == BColor.RED) {
-            MayflowerImage red = new MayflowerImage("src/img/blocks/red-block" + value + ".png");
+            MayflowerImage red = new MayflowerImage("src/img/blocks/red/red-block" + value + ".png");
             red.scale(GameWorld.BLOCK_WIDTH, GameWorld.BLOCK_HEIGHT);
             setImage(red);
         }
