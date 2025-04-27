c52e65725d9a134b5817f48ea3c26bb8ad3bebe4
Nathan Lu <pianobear2008@gmail.com>
2025-04-27 00:38:43 -0500

    randomized blocks synchronized between clients

--- src/Game.java
+++ src/Game.java
@@ -185,7 +185,6 @@ public class Game {
     private void updateBlockVelocities(ArrayList<Block> line, boolean isVertical) {
         double ANIMATION_TIME = 8.5;
         double initialVelocity = 0;
-        // ANIMATION_TIME = 200;
 
         // update velocities of blocks
         for (Block b : line) {
@@ -200,22 +199,12 @@ public class Game {
                         / (ANIMATION_TIME * ANIMATION_TIME);
 
                 b.setAy(acceleration);
-
-                // System.out.println(
-                // "initialPos: " + initialPos + ", finalPos: (" +
-                // b.getTargetDestination().getCol() + ","
-                // + b.getTargetDestination().getRow() + ") acceleration: " + acceleration);
             } else {
                 double initialPos = b.getX();
                 double finalPos = GameWorld.calculateBlockPixel(b.getTargetDestination()).getX();
                 double acceleration = 2 * (finalPos - initialPos) / (ANIMATION_TIME * ANIMATION_TIME);
 
                 b.setAx(acceleration);
-
-                // System.out.println(
-                // "initialPos: " + initialPos + ", finalPos: (" +
-                // b.getTargetDestination().getCol() + ","
-                // + b.getTargetDestination().getRow() + ") acceleration: " + acceleration);
             }
 
             currentlyMovingBlocks.add(b);
@@ -390,27 +379,7 @@ public class Game {
         }
     }
 
-    // public int[] spawnRandomBlock() {
-    //     double spawnValue = Math.random();
-    //     int value;
-    //     if (spawnValue <= 0.7) {
-    //         value = 2;
-    //     } else if (spawnValue <= 0.9) {
-    //         value = 4;
-    //     } else {
-    //         value = 8;
-    //     }
-    //     // Block block = new Block(value, BColor.BLUE, getRandomEmptyTile());
-    //     Coordinate g = getRandomEmptyTile();
-    //     int row = g.getRow();
-    //     int col = g.getCol();
-    //     grid[row][col] = new Block(value, BColor.NEUTRAL);
-
-    //     int[] ret = { row, col, value };
-    //     return ret;
-    // }
-
-    private void printGrid() {
+    public void printGrid() {
         for (int i = 0; i < GRID_HEIGHT; i++) {
             for (int j = 0; j < GRID_WIDTH; j++) {
                 Block b = grid[i][j];
